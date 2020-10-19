48b12cb61a07521ae45ce8f6d735afd4427cd2eb
Amy Paschke <pasch049@umn.edu>
2020-10-18 20:10:01 -0500

    updated high score html page and added new items to JS

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ A timed multiple-choice game used in the browser
 ## Credits
 
 * The background image belongs to Tim Mossholder (@Timmossholder) on Unsplash.
+
+* Base code idea for the High Score HTML page goes to Tiana Rascia via [her website.]("https://www.taniarascia.com/how-to-use-local-storage-with-javascript/?fbclid=IwAR2MO3VrgSL00aX48uTf6lbJqo7P5vYgGz3KBp9yeTbTaZu_ADOP52rRl10")
--- assets/script.js
+++ assets/script.js
@@ -1,80 +1,112 @@
-    let questions = [
-      { text: "Which of these is not a built-in method?", possibleAnswers: ["toUpperCase()", "length()", "indexOf()", "firstName()"], answerIndex: 3 },
-      { text: "What is the style term in JavaScript for when we capitalize the second word in a phrase?", possibleAnswers: ["uppy downy", "camelCase", "capital case", "method writing"], answerIndex: 1 },
-      { text: "What do we put inside an <a> tag to link to an outside source?", possibleAnswers: ["href:", "separate:", "at:", "remove:"], answerIndex: 0 },
-      { text: "What does the 'P' in 'API' stand for?", possibleAnswers: ["position", "possible", "perpetual", "programming"], answerIndex: 3 },
-    ];
+let questions = [
+  { text: "Which of these is not a built-in method?", possibleAnswers: ["toUpperCase()", "length()", "indexOf()", "firstName()"], answerIndex: 3 },
+  { text: "What is the style term in JavaScript for when we capitalize the second word in a phrase?", possibleAnswers: ["uppy downy", "camelCase", "capital case", "method writing"], answerIndex: 1 },
+  { text: "What do we put inside an <a> tag to link to an outside source?", possibleAnswers: ["href:", "separate:", "at:", "remove:"], answerIndex: 0 },
+  { text: "What does the 'P' in 'API' stand for?", possibleAnswers: ["position", "possible", "perpetual", "programming"], answerIndex: 3 },
+];
 
-  document.addEventListener('DOMContentLoaded', setupWorld())
+document.addEventListener('DOMContentLoaded', setupWorld())
 
-  function setupWorld() {
-    state = {
-      currentQuestionIndex: 0,
-      currentScore: 0,
-    }
-
-    scoreElement = document.getElementById("score");
-    startQuizElement = document.getElementById("start-quiz");
-    questionElement = document.getElementById("question")
-    questionTextElement = document.getElementById("question-text")
-    answersElement = document.getElementById("answers");
-    submitScoreElement = document.getElementById("submit-score");
+function setupWorld() {
+  state = {
+    currentQuestionIndex: 0,
+    currentScore: 0,
   }
 
-  function startQuiz() {
-    state.currentQuestionIndex = 0;
+  scoreElement = document.getElementById("score");
+  startQuizElement = document.getElementById("start-quiz");
+  questionElement = document.getElementById("question")
+  questionTextElement = document.getElementById("question-text")
+  answersElement = document.getElementById("answers");
+  submitScoreElement = document.getElementById("submit-score");
+}
 
-    startQuizElement.setAttribute("hidden", true);
+function startQuiz() {
+  state.currentQuestionIndex = 0;
 
+  startQuizElement.setAttribute("hidden", true);
+
+  displayNextQuestion();
+}
+
+function displayNextQuestion() {
+  let currentQuestion = questions[state.currentQuestionIndex];
+
+  questionTextElement.textContent = currentQuestion.text;
+
+  answersElement.innerHTML = "";
+
+  for (i = 0; i < currentQuestion.possibleAnswers.length; i++) {
+    let possibleAnswerButton = document.createElement("BUTTON");
+    possibleAnswerButton.textContent = currentQuestion.possibleAnswers[i];
+    possibleAnswerButton.setAttribute("data-index", i);
+    possibleAnswerButton.onclick = submitAnswer;
+    answersElement.appendChild(possibleAnswerButton);
+  }
+
+  questionElement.removeAttribute("hidden");
+}
+
+function submitAnswer(event) {
+  let submittedAnswerIndex = event.target.dataset.index;
+
+  let currentQuestion = questions[state.currentQuestionIndex];
+
+  if (submittedAnswerIndex == currentQuestion.answerIndex) {
+    awardPoints(10);
+  }
+
+  if (state.currentQuestionIndex >= questions.length - 1) {
+    endQuiz();
+  } else {
+    state.currentQuestionIndex += 1;
     displayNextQuestion();
   }
+}
 
-  function displayNextQuestion() {
-    let currentQuestion = questions[state.currentQuestionIndex];
+function awardPoints(points) {
+  state.currentScore += points;
+  scoreElement.textContent = state.currentScore;
+}
 
-    questionTextElement.textContent = currentQuestion.text;
+function endQuiz() {
+  questionElement.setAttribute("hidden", true);
 
-    answersElement.innerHTML = "";
+  submitScoreElement.removeAttribute("hidden");
+}
 
-    for (i = 0; i < currentQuestion.possibleAnswers.length; i++) {
-      let possibleAnswerButton = document.createElement("BUTTON");
-      possibleAnswerButton.textContent = currentQuestion.possibleAnswers[i];
-      possibleAnswerButton.setAttribute("data-index", i);
-      possibleAnswerButton.onclick = submitAnswer;
-      answersElement.appendChild(possibleAnswerButton);
-    }
+function submitHighScore() {
+  
+}
 
-    questionElement.removeAttribute("hidden");
-  }
+//variables for the high-score html
+let form = document.querySelector("form");
+let submitButton = document.getElementById("score-button");
+let ul = document.querySelector("ul");
+let button = document.querySelector("button");
+let input = document.querySelector("item");
 
-  function submitAnswer(event) {
-    let submittedAnswerIndex = event.target.dataset.index;
+//adds their name to the high score list
+let liMaker = function(text) {
+  let li = document.createElement("li");
+  li.textContent = text;
+  ul.appendChild(li);
+}
 
-    let currentQuestion = questions[state.currentQuestionIndex];
+//this will add the initials (the list items) to the page after the user presses submit
+submitButton.addEventListener("click", function(event) {
+  event.preventDefault();
 
-    if (submittedAnswerIndex == currentQuestion.answerIndex) {
-      awardPoints(10);
-    }
+  let inputValue = document.getElementById("item").value;
+  liMaker(inputValue);
 
-    if (state.currentQuestionIndex >= questions.length - 1) {
-      endQuiz();
-    } else {
-      state.currentQuestionIndex += 1;
-      displayNextQuestion();
-    }
-  }
+  itemsArray.push(input.value);
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+})
 
-  function awardPoints(points) {
-    state.currentScore += points;
-    scoreElement.textContent = state.currentScore;
-  }
+//array will be filled with JSON strings/arrays after we parse through local storage
+let itemsArray = [];
 
-  function endQuiz() {
-    questionElement.setAttribute("hidden", true);
+localStorage.setItem("items", JSON.stringify(itemsArray));
+let data = JSON.parse(localStorage.getItem("items"));
 
-    submitScoreElement.removeAttribute("hidden");
-  }
-
-  function submitHighScore() {
-    
-  }
